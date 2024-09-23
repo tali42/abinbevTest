0b6c3b76cc0744855176b84a1fbdf106f6a09e47
tali42 <tali42>
2024-09-23 23:24:17 +0000

    creating tables and views from silver to gold

--- nb_silver.py
+++ nb_silver.py
@@ -1,2 +1,168 @@
 # Databricks notebook source
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
+# COMMAND ----------
+
+# MAGIC %run ./nb_parametrizacao
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # herdando camada bronze para tratamento
+
+# COMMAND ----------
+
+spark.sql(f"USE {CATALOG_NAME}.{SCHEMA_BRONZE_NAME}")
+
+# COMMAND ----------
+
+brewries_bronze = spark.table("brewries_bronze")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Tratamento de Dados para Silver
+
+# COMMAND ----------
+
+spark.sql(f"USE {CATALOG_NAME}.{SCHEMA_SILVER_NAME}")
+
+# COMMAND ----------
+
+display(brewries_bronze)
+
+# COMMAND ----------
+
+df = brewries_bronze
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Testing unique identifier
+
+# COMMAND ----------
+
+df.count()
+
+# COMMAND ----------
+
+df.distinct().count()
+
+# COMMAND ----------
+
+df.select('id').distinct().count()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## String Columns
+
+# COMMAND ----------
+
+brewries_str_columns = ['id', 'name', 'brewery_type', 'address_1', 'address_2', 'address_3', 'city', 'state_province', 'postal_code', 'country', 'website_url', 'state', 'street', 'phone']
+
+# COMMAND ----------
+
+for column in brewries_str_columns:
+    df = df.withColumn(column, trim(col(column)))
+
+    if ( brewries_bronze.where(col(column)=="").count() > 0):
+        print(f"\nThere is empty str values in column: {column}")
+        df = df.withColumn(column, when(col(column)=="", lit(None)).otherwise(col(column)))
+        print(f"\nEmpty str values in column: {column} Treated!")
+
+    if ( brewries_bronze.where(col(column)==" ").count() > 0):
+        print(f"\nThere is space str values in column: {column}")
+        df = df.withColumn(column, when(col(column)==" ", lit(None)).otherwise(col(column)))
+        print(f"\nEmpty space str in column: {column} Treated!")
+
+    if ( brewries_bronze.where(col(column)=="null").count() > 0):
+        print(f"\nThere is null str values in column: {column}")
+        df = df.withColumn(column, when(col(column)=="null", lit(None)).otherwise(col(column)))
+        print(f"\nEmpty null str in column: {column} Treated!")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ~~Int Column~~
+
+# COMMAND ----------
+
+display(df.select("phone").distinct())
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Decimals
+
+# COMMAND ----------
+
+display(df.select("latitude").distinct())
+
+# COMMAND ----------
+
+display(df.select("longitude").distinct())
+
+# COMMAND ----------
+
+df = (df
+      .withColumn("latitude", col("latitude").cast("Decimal(8,6)"))
+      .withColumn("longitude", col("longitude").cast("Decimal(9,6)"))
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Saving Silver table partitioned
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## exploring possible partitions
+
+# COMMAND ----------
+
+df.count()
+
+# COMMAND ----------
+
+display(df.groupBy("state").agg(count("id").alias("brewries")))
+
+# COMMAND ----------
+
+display(df.groupBy("country").agg(count("id").alias("brewries")))
+
+# COMMAND ----------
+
+display(df.select("state", "state_province").where(col("state")!=col("state_province")))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## INSERT INTO
+
+# COMMAND ----------
+
+columns = ['id',
+'name',
+'brewery_type',
+'address_1',
+'address_2',
+'address_3',
+'street',
+'city',
+'state',
+'postal_code',
+'country',
+'longitude',
+'latitude',
+'phone',
+'website_url']
+
+# COMMAND ----------
+
+df.select(columns) \
+  .write \
+  .mode("overwrite") \
+  .insertInto("brewries_silver")
--- schema/gold_1_0_0.py
+++ schema/gold_1_0_0.py
@@ -5,3 +5,63 @@ from pyspark.sql.types import *
 # COMMAND ----------
 
 # MAGIC %run ../nb_parametrizacao
+
+# COMMAND ----------
+
+spark.sql(f"USE {CATALOG_NAME}.{SCHEMA_GOLD_NAME}")
+
+# COMMAND ----------
+
+print(f"schema_location:{SCHEMA_GOLD_LOCATION}")
+
+# COMMAND ----------
+
+create_locations_sql = f"""
+CREATE TABLE IF NOT EXISTS locations (
+  id_location    STRING       COMMENT 'Location Unique Identifier',
+  city           STRING       COMMENT 'City of the Brewry location',
+  state          STRING       COMMENT 'State of the Brewry location',
+  country        STRING       COMMENT 'Country of the Brewry'
+)
+USING DELTA
+LOCATION '{SCHEMA_GOLD_LOCATION}/locations'
+"""
+
+spark.sql(create_locations_sql)
+
+# COMMAND ----------
+
+create_types_sql = f"""
+CREATE TABLE IF NOT EXISTS types (
+  id_type        STRING       COMMENT 'Brewry Type Unique Identifier',
+  brewery_type   STRING       COMMENT 'Type of the Brewry'
+)
+USING DELTA
+LOCATION '{SCHEMA_GOLD_LOCATION}/types'
+"""
+
+spark.sql(create_types_sql)
+
+# COMMAND ----------
+
+create_brewries_sql = f"""
+CREATE TABLE IF NOT EXISTS brewries (
+  id             STRING       COMMENT 'Brewry Unique Identifier',
+  name           STRING       COMMENT 'Name of the Brewry',
+  address_1      STRING       COMMENT 'First Address Line',
+  address_2      STRING       COMMENT 'Second Address Line',
+  address_3      STRING       COMMENT 'Third Address Line',
+  street         STRING       COMMENT 'Street of the Address',
+  postal_code    STRING       COMMENT 'Postal Code of the Brewry location',
+  longitude      DECIMAL(9,6) COMMENT 'Longitude Data of the Brewry location',
+  latitude       DECIMAL(8,6) COMMENT 'Latitude Data of the Brewry location',
+  phone          STRING       COMMENT 'Phone Number of the Brewry contact',
+  website_url    STRING       COMMENT 'website url of the Brewry',
+  id_location    STRING       COMMENT 'Location Unique Identifier',
+  id_type        STRING       COMMENT 'Brewry Type Unique Identifier'
+)
+USING DELTA
+LOCATION '{SCHEMA_GOLD_LOCATION}/brewries'
+"""
+
+spark.sql(create_brewries_sql)
